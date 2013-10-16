2e1fffeba2aad7e722ae0fc4918bbb004570b329
Pablo <pablo.a.moreno@gmail.com>
2013-10-16 12:39:23 +0100

    Adds test for missing titles and description for PubChem BioAssays retrieved through ESummaries.

--- EUtils/src/test/java/uk/ac/ebi/metabolomes/webservices/eutils/PubChemBioAssayESummaryXMLParserTest.java
+++ EUtils/src/test/java/uk/ac/ebi/metabolomes/webservices/eutils/PubChemBioAssayESummaryXMLParserTest.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.metabolomes.webservices.eutils;
 
+import java.io.InputStream;
 import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,8 +14,27 @@ import java.util.List;
  */
 public class PubChemBioAssayESummaryXMLParserTest {
 
+    /**
+     * Aims to replicate and correct of the behaviour where titles and descriptions of PChem BioAssays where not loaded.
+     *
+     * @throws Exception
+     */
+    @Test
     public void testParseESummaryResultMissingTitleDesc() throws Exception {
         PubChemBioAssayESummaryXMLParser parser = new PubChemBioAssayESummaryXMLParser();
-        //List<PubChemBioAssayESummaryResult> results = parser.parseESummaryResult();
+        InputStream in = PubChemBioAssayESummaryXMLParserTest.class
+                .getResourceAsStream("esummary_pcassay_TitleDesc.xml");
+        List<PubChemBioAssayESummaryResult> results = parser.parseESummaryResult(in);
+
+        for (PubChemBioAssayESummaryResult res : results) {
+            assertNotNull("Result is null",res);
+            String name = res.getName();
+            System.out.println("Name : "+name);
+            assertTrue("Name has length zero",name.replaceAll("'","").trim().length()>0);
+            assertNotNull("Description is null",res.getDescription());
+            String desc = res.getDescription();
+            System.out.println("Description : "+desc+"\n\n");
+            assertTrue("Desc has length zero",desc.replaceAll("'","").trim().length()>0);
+        }
     }
 }
